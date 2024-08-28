61ffec13056b4befb8657c2999581a14c2721ff9
Lucas Freire <lucasfreirel.dev@gmail.com>
2024-08-28 10:40:18 -0300

    Create the Product Properties CRUD

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -6,13 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class dashboardController extends AbstractController
+class DashboardController extends AbstractController
 {
-    #[Route('/dashboard', name: 'app_admin_dashboard')]
+    #[Route('/admin/dashboard', name: 'app_admin_dashboard')]
     public function index(): Response
     {
         return $this->render('admin/dashboard/index.html.twig', [
-            'controller_name' => 'dashboardController',
+            'controller_name' => 'DashboardController',
         ]);
     }
 }
